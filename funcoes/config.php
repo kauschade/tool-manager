ddccf10018bc00c5cc50341d3988e1bf7b5ad4d8
Kau Schade <161056081+kauschade@users.noreply.github.com>
2024-03-09 19:44:39 -0300

    Update config.php

--- funcoes/config.php
+++ funcoes/config.php
@@ -2,6 +2,6 @@
     define('HOST', '127.0.0.1');
     define('USER', 'root');
     define('PASS', '');
-    define('BASE', 'test');
+    define('BASE', 'sistema');
 
     $conn = new MySqli(HOST, USER, PASS, BASE);
